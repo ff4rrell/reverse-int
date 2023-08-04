9206b04c33e8cf0aeb7dea3e5821f727b87cdd12
Saiakbai Nazarbekov <ff4rrell@gmail.com>
2023-08-04 09:37:29 +0600

    feat: add a function to reverse the number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let nToString = String(n);
+    let tempStringFromN = '';
+    for(let i = nToString.length - 1; i >= 0; i--){
+        let word = nToString[i];
+        tempStringFromN += word;
+    }
+
+    return parseFloat(tempStringFromN);
 }
